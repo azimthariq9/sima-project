dca0ce1ac57b7429032eef94470862542fc8a1ee
Marcello <ellobudimanxt@gmail.com>
2026-03-14 15:04:17 +0700

    Mod: move Dosen Controller to API folder

--- app/Http/Controllers/API/DosenController.php
+++ app/Http/Controllers/API/DosenController.php
@@ -22,6 +22,41 @@ class DosenController extends Controller
 
     /*
     |--------------------------------------------------------------------------
+    | VIEW PAGE OF DOSEN
+    |--------------------------------------------------------------------------
+    */
+     public function dashboard()
+    {
+        return view('dosen.dashboard');
+    }
+
+    public function profil()
+    {
+        return view('dosen.profil');
+    }
+
+    public function jadwal()
+    {
+        return view('dosen.jadwal');
+    }
+
+    public function announcement()
+    {
+        return view('dosen.announcement');
+    }
+
+    public function notifikasi()
+    {
+        return view('dosen.notifikasi');
+    }
+
+    public function analytics()
+    {
+        return view('dosen.analytics');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
     | INDEX
     |--------------------------------------------------------------------------
     */
