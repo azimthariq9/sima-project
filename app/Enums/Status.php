ddd0c5beb10db352e32bdedd1ad2b54bca0420f4
ellobudiman <ellobudimanxt@gmail.com>
2026-04-29 12:53:53 +0700

    change name file

---
+++ app/Enums/Status.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+enum Status:string
+{
+    case ACTIVE = 'active';
+    case INACTIVE = 'inactive';
+    case PENDING = 'pending';
+    case DELETED = 'deleted';
+    case REJECTED = 'rejected';
+    case APPROVED = 'approved';
+    case ABSENT = 'absent';
+    case PRESENT = 'present';
+    case EXCUSED = 'excused';
+
+}
